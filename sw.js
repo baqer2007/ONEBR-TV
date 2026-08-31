e18b47db6b488f451463c3633b8bfeb3e73db319
baqer2007 <168880151+baqer2007@users.noreply.github.com>
2026-09-01 00:16:12 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'onebr-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // تمرير الطلبات
+});
